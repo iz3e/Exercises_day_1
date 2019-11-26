89175edda37afff9b32a6a3629cf68e7f7c0ab08
Israel Arias <israelarias55@gmail.com>
2019-11-25 18:33:07 -0600

    Delete: Second loop for

--- triangulo.js
+++ triangulo.js
@@ -1,10 +1,9 @@
-var i,r
+var i
+var l = ''
 function piramide(alto) {
     for(i = 1; i <= alto; i++){
-        for(r = 0; r < i; r++){
-            console.log('#')
-        }
-    console.log('\n')
+        l += '#'
+        console.log(l)
     }
 }
 
