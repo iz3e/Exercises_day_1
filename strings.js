0d0291482b0271d2f0168dc61af5188e838dd6b8
Israel Arias <israelarias55@gmail.com>
2019-11-25 20:49:15 -0600

    Add:Comments

--- strings.js
+++ strings.js
@@ -1,13 +1,18 @@
 const palabra  = "kakkerlak";
+//se usa el split para hacer la palabra un arreglo
 const letras = palabra.split("");
-//console.log(letras);
+//console.log(letras); Solo se uso para ver el resulatdo del arreglo
+// se declara un contador
 var counter = {};
+//se crea un objeto el cual guarda las cantidades 
 letras.forEach(function(letra) {
   if (!counter.hasOwnProperty(letra)) {
     counter[letra] = 0
   }
   counter[letra] += 1;
 });
-//console.log(typeof(counter.a))
+//console.log(typeof(counter.a)) ver tipo de dato 
+// realizamos la suma
 var res = counter.a + counter.e + counter.r +counter.l
+//imprimimos la suma
 console.log('Total de letras: ' + res);
