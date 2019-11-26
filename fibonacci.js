2df5a9668d6e943a3b7c1ec109b7ddc49480bfe2
Israel Arias <israelarias55@gmail.com>
2019-11-25 20:54:45 -0600

    Add:Comments

--- fibonacci.js
+++ fibonacci.js
@@ -1,12 +1,17 @@
 /*
-#Este es un ejemplo que no fue 100%, tome algunos pedazos de codigo
-#que me encontre sobre la recursividad y arme el siguente codigo:
+este es un ejemplo que no fue 100%, tome algunos pedazos de codigo
+que me encontre sobre la recursividad y arme el siguente codigo:
 */
+// declaracion de la variable
 var num = 7
+// inicio de la funcion asi como del loop if
 function fibo(f){
     if(f < 2)
     return f;
+    //se vuelve a llamar a la funcion
     return fibo(f - 1) + fibo(f - 2);
 }
-for(i=0; i<num; i++)
+// imprimimos el resultado con el for
+for(i=0; i<num; i++){
     console.log(fibo(i))
+}
--- triangulo.js
+++ triangulo.js
@@ -1,10 +1,13 @@
+// creacion de las variables
 var i = 1
 var alto = 7  
 var l = ''
+// funcion que va a imprimir el triangulo dependiendo del valor de alto
 const piramide = function(alto) {
     for(i = 1; i <= alto; i++){
         l += '#'
         console.log(l)
     }
 }
+// ejecuccion de la funcion
 console.log(piramide)
